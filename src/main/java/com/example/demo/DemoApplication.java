9ca0f312556d5ef3be8798d8acb8257e671e7fbb
qaz3950339 <18304235659@163.com>
2023-04-19 15:50:52 +0800

    第儿次提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+
+
+        //第二次修改
         SpringApplication.run(DemoApplication.class, args);
     }
 
